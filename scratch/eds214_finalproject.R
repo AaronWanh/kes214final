77e1c45c49da2f834d93538f91a2bee6e0c83109
Karolina Sienko <karolina.e.sienko@gmail.com>
2026-08-25 11:08:34 -0700

    day1 spaghetti code

--- scratch/eds214_finalproject.R
+++ scratch/eds214_finalproject.R
@@ -10,14 +10,23 @@ PRM_data <- read_csv("data/RioMameyesPuenteRoto.csv")
 # Extracting relevant columns (date, concentrations, etc.)
 relevant_bq1 <- bq1_data|> 
   filter(Sample_Date >= "1988-10-18" & Sample_Date <= "1994-04-19") |>  # filtering for certain dates
-  select(Sample_ID, Sample_Date, K, `NO3-N`, Mg, Ca, `NH4-N`)
+  select(Sample_ID, Sample_Date, K, `NO3-N`, Mg, Ca, `NH4-N`) |> 
+  rename(K_bq1 = K, `NO3-N_bq1` = `NO3-N`, Mg_bq1 = Mg, Ca_bq1 = Ca, `NH4-N_bq1` = `NH4-N`)
 
-relevant_bq2 <- bq2_data|> select(Sample_ID, Sample_Date, K, `NO3-N`, Mg, Ca, `NH4-N`)
-relevant_bq3 <- bq3_data|> select(Sample_ID, Sample_Date, K, `NO3-N`, Mg, Ca, `NH4-N`)
-relevant_PRM <- PRM_data|> select(Sample_ID, Sample_Date, K, `NO3-N`, Mg, Ca, `NH4-N`)
+relevant_bq2 <- bq2_data|> 
+  filter(Sample_Date >= "1988-10-18" & Sample_Date <= "1994-04-19") |>  # filtering for certain dates
+  select(Sample_ID, Sample_Date, K, `NO3-N`, Mg, Ca, `NH4-N`) |> 
+  rename(K_bq2 = K, `NO3-N_bq2` = `NO3-N`, Mg_bq2 = Mg, Ca_bq2 = Ca, `NH4-N_bq2` = `NH4-N`)
+
+relevant_bq3 <- bq3_data|> 
+  filter(Sample_Date >= "1988-10-18" & Sample_Date <= "1994-04-19") |>
+  select(Sample_ID, Sample_Date, K, `NO3-N`, Mg, Ca, `NH4-N`) |> 
+  rename(K_bq3 = K, `NO3-N_bq3` = `NO3-N`, Mg_bq3 = Mg, Ca_bq3 = Ca, `NH4-N_bq3` = `NH4-N`)
+
+relevant_PRM <- PRM_data|> select(Sample_ID, Sample_Date, K, `NO3-N`, Mg, Ca, `NH4-N`) |> 
+  rename(K_PRM = K, `NO3-N_PRM` = `NO3-N`, Mg_PRM = Mg, Ca_PRM = Ca, `NH4-N_PRM` = `NH4-N`)
 
 # 9-week moving averages
-
 # window_start
 window_start = seq(ymd("1988-05-20"), ymd("1992-04-19"), by = "9 weeks")
 
